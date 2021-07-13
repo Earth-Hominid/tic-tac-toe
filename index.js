4518d20bbe8766f9db2f0d7ac121cef526d10eb1
C <earthhominid@gmail.com>
2021-07-13 10:26:43 -0300

    placed game in module

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const gameboard = (() => {
   createGrid(3, 3);
 })();
 
-// Player Factory:
-const PlayerFactory = () => {};
-
 // End Game Module:
 const endgame = (() => {
   const endGameDiv = document.querySelector("div.end-game");
@@ -36,107 +33,110 @@ const endgame = (() => {
   createEndGameMessage();
 })();
 
-// Game Logic: 
+// Game Logic:
+const gameLogic = (() => {
+  const xMarkerClass = "x";
+  const oMarkerClass = "o";
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const dataCells = document.querySelectorAll("[data-cell]");
+  const gameBoard = document.getElementById("gameboard");
+  const winningMessageTextElement = document.querySelector(
+    "[data-endgame-text]"
+  );
+  const winningMessage = document.getElementById("endgame");
+  const restartButton = document.getElementById("restartButton");
+  let oTurn;
 
-const xMarkerClass = "x";
-const oMarkerClass = "o";
-const winningCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-const dataCells = document.querySelectorAll("[data-cell]");
-const gameBoard = document.getElementById("gameboard");
-const winningMessageTextElement = document.querySelector("[data-endgame-text]");
-const winningMessage = document.getElementById("endgame");
-const restartButton = document.getElementById("restartButton");
-let oTurn;
+  startGame();
 
-startGame();
+  restartButton.addEventListener("click", startGame);
 
-restartButton.addEventListener("click", startGame);
-
-function startGame() {
-  oTurn = false;
-  dataCells.forEach((cell) => {
-    cell.classList.remove(xMarkerClass);
-    cell.classList.remove(oMarkerClass);
-    cell.removeEventListener("click", setPlayerMove);
-    cell.addEventListener("click", setPlayerMove, { once: true });
-  });
-  setMoveHover();
-  winningMessage.classList.remove("show");
-}
-
-function setPlayerMove(e) {
-  // Select the cell that has been clicked on:
-  const cell = e.target;
-
-  //If it is circles turn, return circle class, otherwise return x class:
-  const currentClass = oTurn ? oMarkerClass : xMarkerClass;
-  placePlayerMove(cell, currentClass);
-
-  // Check for a winner:
-  if (checkForWinner(currentClass)) {
-    endGame(false);
-  } else if (isDraw()) {
-    endGame(true);
-  } else {
-    switchTurns();
+  function startGame() {
+    oTurn = false;
+    dataCells.forEach((cell) => {
+      cell.classList.remove(xMarkerClass);
+      cell.classList.remove(oMarkerClass);
+      cell.removeEventListener("click", setPlayerMove);
+      cell.addEventListener("click", setPlayerMove, { once: true });
+    });
     setMoveHover();
+    winningMessage.classList.remove("show");
   }
 
-  function endGame(draw) {
-    if (draw) {
-      winningMessageTextElement.innerText = "It's a Tie!";
+  function setPlayerMove(e) {
+    // Select the cell that has been clicked on:
+    const cell = e.target;
+
+    //If it is circles turn, return circle class, otherwise return x class:
+    const currentClass = oTurn ? oMarkerClass : xMarkerClass;
+    placePlayerMove(cell, currentClass);
+
+    // Check for a winner:
+    if (checkForWinner(currentClass)) {
+      endGame(false);
+    } else if (isDraw()) {
+      endGame(true);
     } else {
-      winningMessageTextElement.innerText = `${oTurn ? "O" : "X"} Wins!`;
+      switchTurns();
+      setMoveHover();
     }
-    winningMessage.classList.add("show");
+
+    function endGame(draw) {
+      if (draw) {
+        winningMessageTextElement.innerText = "It's a Tie!";
+      } else {
+        winningMessageTextElement.innerText = `${oTurn ? "O" : "X"} Wins!`;
+      }
+      winningMessage.classList.add("show");
+    }
+
+    function isDraw() {
+      // Destructure dataCells into an array in order to use 'every method':
+      return [...dataCells].every((cell) => {
+        return (
+          cell.classList.contains(xMarkerClass) ||
+          cell.classList.contains(oMarkerClass)
+        );
+      });
+    }
+
+    //Create function to set the player's move on board:
+
+    function placePlayerMove(cell, currentClass) {
+      cell.classList.add(currentClass);
+    }
+
+    // Check for a draw:
+    // Switch Player Turn:
+    function switchTurns() {
+      oTurn = !oTurn;
+    }
   }
 
-  function isDraw() {
-    // Destructure dataCells into an array in order to use 'every method':
-    return [...dataCells].every((cell) => {
-      return (
-        cell.classList.contains(xMarkerClass) ||
-        cell.classList.contains(oMarkerClass)
-      );
+  function setMoveHover() {
+    gameBoard.classList.remove(xMarkerClass);
+    gameBoard.classList.remove(oMarkerClass);
+    if (oTurn) {
+      gameBoard.classList.add(oMarkerClass);
+    } else {
+      gameBoard.classList.add(xMarkerClass);
+    }
+  }
+
+  function checkForWinner(currentClass) {
+    return winningCombos.some((combination) => {
+      return combination.every((index) => {
+        return dataCells[index].classList.contains(currentClass);
+      });
     });
   }
-
-  //Create function to set the player's move on board:
-
-  function placePlayerMove(cell, currentClass) {
-    cell.classList.add(currentClass);
-  }
-
-  // Check for a draw:
-  // Switch Player Turn:
-  function switchTurns() {
-    oTurn = !oTurn;
-  }
-}
-
-function setMoveHover() {
-  gameBoard.classList.remove(xMarkerClass);
-  gameBoard.classList.remove(oMarkerClass);
-  if (oTurn) {
-    gameBoard.classList.add(oMarkerClass);
-  } else {
-    gameBoard.classList.add(xMarkerClass);
-  }
-}
-
-function checkForWinner(currentClass) {
-  return winningCombos.some((combination) => {
-    return combination.every((index) => {
-      return dataCells[index].classList.contains(currentClass);
-    });
-  });
-}
+})();
